e13c46ae8ee870f5e1069cf3a432eaf5f65aae81
Viktoria Bors-Pajuste <viktoria.pajuste@gmail.com>
2022-10-28 12:09:25 +0200

    vue cli

---
+++ Vue/udemy/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsVisible">
+        <li><strong>Phone:</strong>{{friend.phone}}</li>
+         <li><strong>E-mail:</strong>{{friend.email}}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+    data (){
+        return {
+            detailsVisible: false,
+            friend: 
+                 {id:'manual', name:"Manual Lorenz", phone:'123 123 123', email: 'asd@asd.com'}
+        }
+    },
+    methods:{
+        toggleDetails(){
+            this.detailsVisible = !this.detailsVisible
+        }
+    }
+}
+</script>
+
+<style>
+li{
+    list-style: none;
+}
+</style>
